85028e0a28939f9d05efa70f509ed977ce4a6d03
Greg Zimmerman <thezim@gmail.com>
2017-08-20 15:51:29 -0400

    Initial module.

---
+++ posh-nuget.psm1
@@ -0,0 +1,35 @@
+﻿function Add-TypeFromNuget {
+    [CmdletBinding()]
+    param(
+        $Uri="https://www.nuget.org/api/v2/package",
+        [ValidateNotNullOrEmpty()]
+        [string]$Package,
+        [ValidateNotNullOrEmpty()]
+        [string[]]$Library
+    )
+    [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression") | Out-Null
+    $response = Invoke-WebRequest -Uri "$Uri/$Package"
+    $zip = New-Object System.IO.Compression.ZipArchive($response.RawContentStream)
+    foreach($lib in $Library){
+        $entry = $zip.GetEntry($lib)
+        $entryreader = New-Object System.IO.StreamReader($entry.Open())
+        $ms = New-Object System.IO.MemoryStream
+        $entryreader.BaseStream.CopyTo($ms, $entry.Length)
+        $entryreader.Close()
+        [Reflection.Assembly]::Load($ms.ToArray()) | Out-Null
+        $ms.Close()
+    }
+}
+
+function Get-NugetPackage {
+    [CmdletBinding()]
+    param(
+        $Uri="https://www.nuget.org/api/v2/package",
+        [ValidateNotNullOrEmpty()]
+        [string]$Package
+    )
+    [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression") | Out-Null
+    $response = Invoke-WebRequest -Uri "$Uri/$Package"
+    $zip = New-Object System.IO.Compression.ZipArchive($response.RawContentStream)
+    $zip.Entries
+}
